b53f49c6ba0f85cf6977244bac22f971ba988ea2
Raheem Azeez Abiodun <raheemazeez4@gmail.com>
2025-01-30 20:08:22 +0000

    Bootstrap the main application

---
+++ deep_medical/exceptions.py
@@ -0,0 +1,6 @@
+class AppException(Exception):
+    pass
+
+
+class NotFoundException(AppException):
+    pass
---
+++ main.py
@@ -0,0 +1,20 @@
+from fastapi import FastAPI, status, Request
+from fastapi.responses import JSONResponse
+
+from deep_medical.exceptions import AppException, NotFoundException
+
+app = FastAPI(
+    title="Task Manager API",
+    version="1.0",
+    summary="The APIs to serve the task manager based on the deep medical specification"
+)
+
+
+@app.exception_handler(AppException)
+async def app_exception_handler(request: Request, exc: AppException) -> JSONResponse:
+    if isinstance(exc, NotFoundException):
+        status_code = status.HTTP_404_NOT_FOUND
+    else:
+        status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+
+    return JSONResponse({"detail": str(exc)}, status_code=status_code)
